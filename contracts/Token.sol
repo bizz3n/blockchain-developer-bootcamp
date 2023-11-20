e30f82e72cab5fef2b4d423be6c26a1463ade111
Ben Castilla <ben@castilla.us>
2023-11-20 15:46:20 -0800

    3. Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,20 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Token {
-	string public name = "My Token";
+	string public name;
+	string public symbol;
+	//Decimals
+	uint256 public decimals = 18;
+	//Total Supply
+	uint256 public totalSupply;
+	// uint256 public totalSupply = 1000000 * (10**decimals); // 1,000,000 x10^18;
+
+
+	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+		name = _name;
+		symbol = _symbol;
+		totalSupply = _totalSupply * (10**decimals); // 1,000,000 * 10*18;
+	}
+
 }
+
