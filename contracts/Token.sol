cc48406543a30e353cfd0108219bdf2e5d82c713
Ben Castilla <ben@castilla.us>
2023-11-22 05:08:06 -0800

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,17 +6,24 @@ import "hardhat/console.sol";
 contract Token {
 	string public name;
 	string public symbol;
-	//Decimals
 	uint256 public decimals = 18;
-	//Total Supply
 	uint256 public totalSupply;
 	// uint256 public totalSupply = 1000000 * (10**decimals); // 1,000,000 x10^18;
+
+	//Track Balance
+	mapping(address => uint256) public balanceOf;
+	//Send Tokens
+
+
 
 
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
 		name = _name;
 		symbol = _symbol;
-		totalSupply = _totalSupply * (10**decimals); // 1,000,000 * 10*18;
+		totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
+
+
 	}
 
 }
