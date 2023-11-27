313109f00ea79b106bb47452f6d5d9df03176e4f
Ben Castilla <ben@castilla.us>
2023-11-27 12:00:53 -0800

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,11 +12,20 @@ contract Token {
 
 	//Track Balance
 	mapping(address => uint256) public balanceOf;
+
+	mapping(address => mapping(address => uint256)) public allowance;
+
 	//Send Tokens
 
 	event Transfer(
 		address indexed from,
 		address indexed to,
+		uint256 value
+	);
+
+	event Approval(
+		address indexed owner, 
+		address indexed spender, 
 		uint256 value
 	);
 
@@ -46,6 +55,18 @@ contract Token {
 		balanceOf[_to] = balanceOf[_to] + _value;
 
 		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	function approve(address _spender, uint256 _value) 
+	public 
+	returns(bool success)
+	{
+		require(_spender != address(0));
+		
+		allowance[msg.sender][_spender] = _value;
+
+		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 
